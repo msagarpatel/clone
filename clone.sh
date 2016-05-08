91865d1e8d79053284b34d9455560c6ce2c2041a
Sagar Patel <sagarmp@live.unc.edu>
2016-05-08 02:50:46 -0400

    version 1.2

--- clone.sh
+++ clone.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 # Author        :   Sagar Patel
-# Version       :   1.1
+# Version       :   1.2
+# Date          :   May 8, 2016
 # What?         :   This little script here is used to create a bootable copy of your boot drive. This will clone the system root folder (i.e. '/') to '$DEST'. You can change '$DEST' below to whatever drive you want.
 
-# What's New?   :   -Adding the ability to have shutdown after cloning optional by passing arguments (i.e. '-s')
-#                   -Adding the 'caffeinate' thing here rather than the '.profile' file.
-#                   -Play "CLONE WARS.m4a". (It won't play if not present. Also, it's optional with '-m'.)
-#                   -Now checking if the OS is OS X.
-#                   -Check version with '-v'.
-#                   -Adding a introductory 'Star Wars' music with title.
-#                   -You can now kill the entire script with 'Ctrl+C'.
+# What's New?   :   -Added the version thing as a variable so I don't have to go around and change every verison number during a new version. Also changed the method of showing the version into a function.
+#                   -Using 'getopts' to process options now.
+#                   -Now checking if the '$EXCLUDE_FILE' is present or not.
+#                   -Redirects 'kill' messages to '/dev/null'. Basically not showing them on the terminal screen.
+#                   -Moved the 'trap' further up becuase way not?!
+#                   -Added a delay before ejecting '$DEST' becuase it was somehow always busy right after 'rsync'.
 
 # VARIABLES
 DEST="/Volumes/SSSD0/"
@@ -19,36 +19,60 @@ EXCLUDE_FILE="/Users/sagarpatel/bin/rsync_excludes.txt"
 shutdownTimeout=1
 shutdownOnCompletion=true
 playMusic=true
+VERSION="CLONE WARS v1.2"
 
 clear
 
-# Checking and setting the passed arguments
-while [[ $# -ne 0 ]]; do
-    case $1 in
+# used to 'trap' and kill the entire script with Ctrl+C. I have no idea how it works.
+# ref. 12 & 13
+exiting () {
+echo "Well, now that you want to exit... BYE."
+kill $afplayPID 2> /dev/null
+kill $caffeinatePID 2> /dev/null
+exit 7
+}
+trap exiting INT
 
-        -m)
+# show the version in a ridiculous way
+showVersion() {
+echo
+echo " ###### #      ###### #     # ######   #             #    #     ###### ###### "
+echo " #      #      #    # ##    # #         #     #     #    # #    #    # #      "
+echo " #      #      #    # #  #  # #####      #   # #   #    #   #   ###### ###### "
+echo " #      #      #    # #    ## #           # #   # #    # ### #  # ##        # "
+echo " ###### ###### ###### #     # ######       #     #    #       # #   ## ###### "
+echo
+echo $VERSION
+echo
+}
+
+# Checking and setting the passed arguments
+# ref. 14
+# OLD : while [[ $# -ne 0 ]]; do
+while getopts msv option; do
+    case $option in
+
+        m)
         playMusic=false
         echo "Okay, no music. You're such an bore-snore."
         ;;
 
-        -s)
+        s)
         shutdownOnCompletion=false
         echo "The system will not shut down on completion of cloning."
         ;;
 
-        -v)
-        echo "CLONE WARS v1.1"
+        v)
+        showVersion
         exit 5
         ;;
 
         *)
         echo "==========ERROR 3=========="
-        echo "$1 is an invalid argument."
         exit 3
         ;;
 
     esac
-    shift
 done
 
 # ref. 4
@@ -71,6 +95,14 @@ if [ ! -f "$CLONE_WARS" ]; then
     echo "$CLONE_WARS not found, but that's cool we won't play it..."
 fi
 
+if [ ! -f "$EXCLUDE_FILE" ]; then
+    echo "==========ERROR 8=========="
+    echo "$EXCLUDE_FILE not found."
+    echo "This file tells 'rsync' what not to copy from the hard drive."
+    echo "If not present, it will copy every file on the hard drive, which is not recommended."
+    exit 8
+fi
+
 # ref. 10
 # check to see of the OS is OS X (Darwin), as the script only supports OS X
 if [ $(uname) != "Darwin" ]; then
@@ -79,34 +111,12 @@ if [ $(uname) != "Darwin" ]; then
     exit 6
 fi
 
-# used to 'trap' and kill the entire script with Ctrl+C. I have no idea how it works.
-# ref. 12 & 13
-exiting () {
-    echo "Well, now that you want to exit... BYE."
-    kill $afplayPID 2> /dev/null
-    kill $caffeinatePID 2> /dev/null
-#
-#
-# OVER HERE! UP THERE! the send stuff to /dev/null
-#
-#
-    exit 7
-}
-trap exiting INT
-
 # play the 'CLONE WARS' music
 if [ $playMusic = "true" ]; then
     for i in {1..3}; do echo -n ". "; sleep 0.5; done
+    clear
+    showVersion
     afplay "$CLONE_WARS" & afplayPID=$!
-    echo
-    echo " ###### #      ###### #     # ######   #             #    #     ###### ###### "
-    echo " #      #      #    # ##    # #         #     #     #    # #    #    # ##     "
-    echo " #      #      #    # #  #  # #####      #   # #   #    #   #   ###### ###### "
-    echo " #      #      #    # #    ## #           # #   # #    # ### #  # ##       ## "
-    echo " ###### ###### ###### #     # ######       #     #    #       # #   ## ###### "
-    echo
-    echo "CLONE WARS v1.1"
-    echo
 fi
 
 # prevent system from going into idle sleep (using 'caffeinate' instead of 'pmset idle' because it is deprecated in favour of the former.)
@@ -117,8 +127,9 @@ caffeinate -i & caffeinatePID=$!
 # UPDATE : It works good, seems like it'll be okay without '-i'.
 sudo rsync -vaxE --progress --delete --exclude-from="$EXCLUDE_FILE" / "$DEST"
 echo "==========Finished copying=========="
-echo "==========Blessing the CoreServices...=========="
 sudo bless -folder "$DEST"/System/Library/CoreServices && echo "==========Blessed the CoreServices.=========="
+echo "Just a bit more time..."
+sleep 5
 diskutil eject "$DEST"
 kill "$caffeinatePID"
 
@@ -171,6 +182,9 @@ exit 0
 # 13. Exit traps
 # http://redsymbol.net/articles/bash-exit-traps/
 
+# 14. Using 'getopts' for extracting options from parsed arguments
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+
 # EXIT/ERROR CODES
 # 0 : Cloned Successfully
 # 1 : The destination volume, $DEST, was not present. (Note: $DEST is the destination volume)
@@ -180,3 +194,4 @@ exit 0
 # 5 : Printed version number and exited.
 # 6 : The script only supports OS X (Darwin)
 # 7 : The script was killed while working.
+# 8 : '$EXCLUDE_FILE' was not found at the set location.
